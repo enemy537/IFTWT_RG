e72dab3980a29da6669c49511786666dc9b2bae7
Pedro Paiva <enemy537@gmail.com>
2018-08-14 14:12:30 -0300

    Optmization on graph operations

--- graph.hpp
+++ graph.hpp
@@ -2,18 +2,296 @@
 // Created by avell on 07/05/18.
 //
 #include <pcl/pcl_base.h>
-#include <pcl/octree/octree.h>
 #include <pcl/octree/octree_pointcloud_adjacency.h>
-#include <pcl/segmentation/supervoxel_clustering.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/copy.hpp>
+#include <vtkRenderWindow.h>
+#include <vtkCubeSource.h>
+#include <vtkCleanPolyData.h>
 
-template <typename PointT, typename NormalT>
-class Graph : public pcl::Supervoxel<PointT>{
+typedef pcl::PointXYZI PointI;
+typedef pcl::PointCloud<PointI> CloudI;
+typedef boost::adjacency_list<boost::setS, boost::setS, boost::undirectedS, PointI, float> graph_t;
+enum class MORPH : char {bin_erode = 1,bin_dilate = 2,dilate = 3,erode = 4};
+
+CloudI::Ptr g_to_pc(graph_t& g_)
+{
+    auto deep_copy = [](const PointI& p1){
+        PointI p (p1.intensity);
+        p.x = p1.x; p.y = p1.y; p.z = p1.z;
+        return p;
+    };
+
+    CloudI::Ptr out (new CloudI());
+    using vd = typename graph_t::vertex_descriptor;
+    const auto vd_v = vertices(g_);
+    for (auto v = vd_v.first; v != vd_v.second; ++v) {
+
+        out->push_back(deep_copy(g_[*v]));
+    }
+    return out;
+};
+class Graph {
 public:
-    Graph<PointT>() :
-        cloud_ (cloud)
-    {};
-protected:
-    pcl::PointCloud<PointT>::Ptr cloud_;
-private:
+    Graph(pcl::PointCloud<PointI>::Ptr cloud) : voxel_size(0),overlap(9.e+2),
+                                                tree_(new pcl::search::KdTree<PointI>),
+                                                octree_(nullptr)
+    {
+        cloud_ = cloud;
+        tree_->setInputCloud(cloud_);
+        initialize();
+    };
+    Graph(pcl::PointCloud<PointI>::Ptr cloud, double ovrlp) : voxel_size(0),
+                                                tree_(new pcl::search::KdTree<PointI>),
+                                                octree_(nullptr)
+    {
+        overlap = ovrlp;
+        cloud_ = cloud;
+        tree_->setInputCloud(cloud_);
+        initialize();
+    };
+    ~Graph(){ }
+    void setInputCloud(pcl::PointCloud<PointI >::Ptr cloud)
+    {
+        cloud_ = cloud;
+        tree_->setInputCloud (cloud_);
+        initialize();
+    };
+    void setOverlap(double overlap)
+    {
+        this->overlap = overlap;
+        initialize();
+    }
+    graph_t getAdjacencyList()
+    {
+        return g_;
+    };
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> voxelViewer()
+    {
+        //     Voxel visualization stuff
+        typedef std::vector< PointI, Eigen::aligned_allocator<PointI> > AlignedPointTVector;
+        AlignedPointTVector voxelCenters;
 
+        voxelCenters.clear();
+
+        octree_->getOccupiedVoxelCenters(voxelCenters);
+        double voxelSideLen = std::sqrt(octree_->getVoxelSquaredSideLen());
+
+        boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+
+        // Draw Voxels
+
+        vtkSmartPointer<vtkAppendPolyData> appendFilter = vtkSmartPointer<vtkAppendPolyData>::New ();
+
+        // Create every cubes to be displayed
+
+        for(int i = 0; i < voxelCenters.size (); i++) {
+            auto s = static_cast<float> (voxelSideLen / 2.0);
+            float x = voxelCenters[i].x;
+            float y = voxelCenters[i].y;
+            float z = voxelCenters[i].z;
+
+            vtkSmartPointer<vtkCubeSource> wk_cubeSource = vtkSmartPointer<vtkCubeSource>::New();
+
+            wk_cubeSource->SetBounds(x - s, x + s, y - s, y + s, z - s, z + s);
+            wk_cubeSource->Update();
+
+            appendFilter->AddInputData(wk_cubeSource->GetOutput());
+        }
+        // Remove any duplicate points
+        vtkSmartPointer<vtkCleanPolyData> cleanFilter = vtkSmartPointer<vtkCleanPolyData>::New ();
+
+        cleanFilter->SetInputConnection (appendFilter->GetOutputPort ());
+        cleanFilter->Update ();
+
+        //Create a mapper and actor
+        vtkSmartPointer<vtkPolyDataMapper> multiMapper = vtkSmartPointer<vtkPolyDataMapper>::New ();
+
+        multiMapper->SetInputConnection (cleanFilter->GetOutputPort ());
+
+        vtkSmartPointer<vtkActor> multiActor = vtkSmartPointer<vtkActor>::New ();
+
+        multiActor->SetMapper (multiMapper);
+
+        multiActor->GetProperty ()->SetColor (1.0, 1.0, 1.0);
+        multiActor->GetProperty ()->SetAmbient (1.0);
+        multiActor->GetProperty ()->SetLineWidth (0.1);
+        multiActor->GetProperty ()->EdgeVisibilityOn ();
+        multiActor->GetProperty ()->SetOpacity (1.0);
+
+        multiActor->GetProperty ()->SetRepresentationToWireframe ();
+
+        viewer->getRenderWindow()->GetRenderers()->GetFirstRenderer()->AddActor(multiActor);
+
+        viewer->addPointCloud<PointI>(cloud_, "color_cloud");
+        viewer->setBackgroundColor (0, 0, 0);
+        viewer->setPointCloudRenderingProperties(
+                pcl::visualization::PCL_VISUALIZER_POINT_SIZE,3,"color_cloud");
+        return viewer;
+    };
+    CloudI::Ptr morph_bin_erode(CloudI::Ptr out)
+    {
+        return morph_base(MORPH::bin_erode,out);
+    }
+    CloudI::Ptr morph_bin_dilate(CloudI::Ptr out)
+    {
+        return morph_base(MORPH::bin_dilate,out);
+    }
+    CloudI::Ptr morph_erode(CloudI::Ptr out)
+    {
+        return morph_base(MORPH::erode,out);
+    }
+    CloudI::Ptr morph_dilate(CloudI::Ptr out)
+    {
+        return morph_base(MORPH::dilate,out);
+    }
+//    CloudI::Ptr morph_gradient()
+//    {
+//        CloudI::Ptr cloud_e = this->morph_erode();
+//        CloudI::Ptr cloud_d = this->morph_dilate();
+//        CloudI::Ptr cloud_g (new CloudI);
+//
+//        for(int i = 0; i < cloud_g->size(); i++){
+//            float intensity = cloud_d->points[i].intensity
+//                            - cloud_e->points[i].intensity;
+//            cloud_g->points[i].intensity = intensity;
+//        }
+//        return cloud_g;
+//    }
+
+protected:
+    double
+    computeCloudResolution ()
+    {
+        // Finding optimum voxel size
+        double res = 0.0;
+        int n_points = 0;
+        int nres;
+        std::vector<int> indices (2);
+        std::vector<float> sqr_distances (2);
+
+        for (size_t i = 0; i < cloud_->size (); ++i)
+        {
+            if (! pcl_isfinite ((*cloud_)[i].x))
+            {
+                continue;
+            }
+            //Considering the second neighbor since the first is the point itself.
+            nres = tree_->nearestKSearch (i, 2, indices, sqr_distances);
+            if (nres == 2)
+            {
+                res += sqrt (sqr_distances[1]);
+                ++n_points;
+            }
+        }
+        if (n_points != 0)
+        {
+            res /= n_points;
+            // Overlap adjust
+            res *= overlap;
+        }
+        return res;
+    }
+    int find_p(pcl::search::KdTree<PointI>::Ptr tree, const PointI& p)
+    {
+        std::vector<int> nn_i (1);
+        std::vector<float> nn_d (1);
+        tree->nearestKSearch(p,1, nn_i, nn_d);
+        return nn_i[0];
+    }
+    void optimizeGraph()
+    {
+        const auto vd_v = vertices(g_);
+        for(auto v = vd_v.first; v != vd_v.second; ++v)
+        {
+            auto nn_i = find_p(tree_,g_[*v]);
+            g_[*v].intensity = cloud_->points[nn_i].intensity;
+        }
+    }
+private:
+    pcl::PointCloud<PointI>::Ptr cloud_;
+    pcl::search::KdTree<PointI>::Ptr tree_;
+    double voxel_size, overlap;
+    graph_t g_;
+    pcl::octree::OctreePointCloudAdjacency<PointI>::Ptr octree_;
+
+    void initialize()
+    {
+        voxel_size = computeCloudResolution();
+        // Compute the octree adjacency tree
+        octree_.reset(new pcl::octree::OctreePointCloudAdjacency<PointI>(voxel_size));
+        octree_->setInputCloud(cloud_);
+        octree_->addPointsFromInputCloud();
+
+        std::cout << "octree_size " << octree_->size() << std::endl;
+        std::cout << "cloud_size " << cloud_->size() << std::endl;
+
+        // Adjacency Graph
+        octree_->computeVoxelAdjacencyGraph(g_);
+        optimizeGraph();
+        cloud_.reset();
+    };
+    graph_t copy_g()
+    {
+        std::map<graph_t::vertex_descriptor, int> index;
+        for (auto v : boost::make_iterator_range(boost::vertices(g_))) {
+            index.insert(std::make_pair(v, index.size()));
+        }
+        graph_t g_out;
+        boost::copy_graph(g_,g_out,
+        boost::vertex_index_map(boost::make_assoc_property_map(index)));
+        return g_out;
+    }
+    CloudI::Ptr morph_base(MORPH type, CloudI::Ptr& out){
+        graph_t g_in = g_;
+        out->swap(*g_to_pc(g_));
+        pcl::search::KdTree<PointI>::Ptr tree (new pcl::search::KdTree<PointI>);
+        tree->setInputCloud(out);
+        const auto vd_g = vertices(g_in);
+#pragma omp parallel
+        {
+#pragma omp single
+            {
+                for (auto v = vd_g.first; v != vd_g.second; ++v) {
+#pragma omp task shared(out)
+                    {
+                        const auto adj_v = boost::adjacent_vertices(*v, g_in);
+                        using vd = typename graph_t::vertex_descriptor;
+                        float intensity = 0;
+
+                        if(type == MORPH::bin_erode){
+                            bool erodible = std::find_if(adj_v.first, adj_v.second,
+                                                         [&g_in](const vd &d) { return g_in[d].intensity == 0; }
+                            ) != adj_v.second;
+                            intensity = erodible ? 0.f : 255.f;
+                        }else if(type == MORPH::bin_dilate){
+                            bool dilatable = std::find_if(adj_v.first, adj_v.second,
+                                                          [&g_in](const vd &d) { return g_in[d].intensity == 255;}
+                            ) != adj_v.second;
+                            float intensity = dilatable ? 255.f:0.f;
+                        }else if(type == MORPH::erode){
+                            intensity = 255;
+                            for(auto i = adj_v.first; i!=adj_v.second; i++){
+                                float temp_i = g_in[*i].intensity;
+                                if(temp_i < intensity) intensity = temp_i ;
+                            }
+                        }else if(type == MORPH::dilate){
+                            intensity = 0;
+                            for(auto i = adj_v.first; i!=adj_v.second; i++){
+                                float temp_i = g_in[*i].intensity;
+                                if(temp_i > intensity) intensity = temp_i ;
+                            }
+                        }
+
+                        int id_out = find_p(tree,g_in[*v]);
+                        out->points[id_out].intensity = intensity;
+                    }
+                }
+            }
+        }
+        return out;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -2,26 +2,19 @@
 #include <vector>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl/search/search.h>
-#include <pcl/search/kdtree.h>
-#include <pcl/features/normal_3d_omp.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/filters/extract_indices.h>
-#include "flat_point.hpp"
 #include <pcl/octree/octree_pointcloud_adjacency.h>
-#include <pcl/filters/voxel_grid.h>
-#include <vtkRenderWindow.h>
-#include <vtkCubeSource.h>
-#include <vtkCleanPolyData.h>
 #include <exercices_toolbox.h>
 #include <opencv2/opencv.hpp>
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <pcl/kdtree/flann.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <opencv2/imgproc/types_c.h>
+#include "graph.hpp"
 
 typedef pcl::PointXYZRGB PointT;
+typedef pcl::PointXYZI PointI;
 typedef pcl::PointCloud<PointT> CloudT;
+typedef pcl::PointCloud<PointI> CloudI;
 
 pcl::PointCloud<pcl::PointXYZI>::Ptr cloudRGB2GRAY(pcl::PointCloud<PointT>::Ptr cloud)
 {
@@ -62,67 +55,57 @@ pcl::PointCloud<pcl::PointXYZI>::Ptr cloudRGB2GRAY(pcl::PointCloud<PointT>::Ptr 
 
     return cloud_gray;
 }
-typedef pcl::PointXYZI Point;
-float get_intensity_at(const pcl::PointCloud<Point>::Ptr cloud, Point& p){
-    pcl::KdTreeFLANN<pcl::PointXYZI> tree;
-    tree.setInputCloud(cloud);
-    std::vector<int> nn_indices (1);
-    std::vector<float> nn_dists (1);
-
-    tree.nearestKSearch(p,1, nn_indices, nn_dists);
-
-    return cloud->points[nn_indices[0]].intensity;;
-}
-typedef boost::adjacency_list<boost::setS, boost::setS, boost::undirectedS, pcl::PointXYZI, float> Graph;
-typedef pcl::PointXYZI Point;
-pcl::PointCloud<Point>::Ptr erode_cloud(Graph& g, pcl::PointCloud<Point>::Ptr c)
+pcl::PointCloud<pcl::PointXYZI>::Ptr cloudGray(pcl::PointCloud<PointT>::Ptr cloud)
 {
-    using vd = typename Graph::vertex_descriptor;
-    pcl::PointCloud<Point>::Ptr out (new pcl::PointCloud<Point>);
-    auto deep_copy = [](const Point& p1){
-        Point p (p1.intensity);
-        p.x = p1.x; p.y = p1.y; p.z = p1.z;
-        return p;
-    };
-    const auto vd_v = vertices(g);
-    for(auto v = vd_v.first; v != vd_v.second; ++v)
-    {
-        const auto adj_v = boost::adjacent_vertices(*v,g);
-        bool erodible = std::find_if(adj_v.first,adj_v.second,
-                                 [&g,&c](const vd& d)
-                                 {
-                                 return get_intensity_at(c,g[d]) == 0;
-                                 }
-                                 )!= adj_v.second;
-        int intensity = erodible ? 0:255;
-        pcl::PointXYZI p = deep_copy(g[*v]);
-        p.intensity = intensity;
-        out->push_back(p);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_gray (new pcl::PointCloud<pcl::PointXYZI>);
+    cloud_gray->height = cloud->height;
+    cloud_gray->width = cloud->width;
+
+    for(pcl::PointCloud<PointT>::iterator it = cloud->begin(); it != cloud->end(); it++){
+        // Color conversion
+        cv::Mat pixel(1,1,CV_8UC3,cv::Scalar(it->r,it->g,it->b));
+        cv::Mat temp;
+        cv::cvtColor(pixel,temp,CV_RGB2GRAY);
+
+        pcl::PointXYZI pointI;
+        pointI.x = it->x;
+        pointI.y = it->y;
+        pointI.z = it->z;
+        pointI.intensity = temp.at<uchar>(0,0);
+
+        cloud_gray->push_back(pointI);
     }
-    return out;
+    return cloud_gray;
 }
 
 int main (int argc, char** argv) {
     CloudT::Ptr cloud_raw (new CloudT());
-    CloudT::Ptr cloud (new CloudT());
-    if ( pcl::io::loadPCDFile <PointT> ("/home/avell/Desktop/point_cloud/lucy.pcd", *cloud_raw) == -1) {
+    if ( pcl::io::loadPCDFile <PointT> ("/home/pedro/Desktop/point_cloud/boamorte_final.pcd", *cloud_raw) == -1) {
         std::cout << "Cloud reading failed." << std::endl;
         return (-1);
     }
 
-    // Cloud normalization
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_i = cloudGray(cloud_raw);
 
-    pcl::VoxelGrid<PointT> downsampler;
-    downsampler.setInputCloud(cloud_raw);
-    downsampler.setLeafSize(.006f,.006f,.006f);
-    downsampler.filter(*cloud);
+    Graph gg (cloud_i,10);
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_i = cloudRGB2GRAY(cloud);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_e (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_d (new pcl::PointCloud<pcl::PointXYZI>);
+    gg.morph_erode(cloud_e);
+    gg.morph_dilate(cloud_d);
 
-    // Octree adjacency
-    pcl::octree::OctreePointCloudAdjacency<pcl::PointXYZI> octree(.008f);
-    octree.setInputCloud(cloud_i);
-    octree.addPointsFromInputCloud();
+    CloudI::Ptr cloud_g (new CloudI);
+    pcl::copyPointCloud(*cloud_d,*cloud_g);
+
+    for(int i = 0; i < cloud_g->size(); i++){
+        float intensity = cloud_d->points[i].intensity
+                          - cloud_e->points[i].intensity;
+        cloud_g->points[i].intensity = intensity;
+    }
+
+//    pcl::io::savePCDFile("lucy_gradient.pcd",*cloud_g);
+//    pcl::io::savePCDFile("lucy_erode.pcd",*cloud_e);
+//    pcl::io::savePCDFile("lucy_dilate.pcd",*cloud_d);
 
     // Project IFTWT_RG
     /**pcl::search::Search<pcl::PointXYZ>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZ> > (new pcl::search::KdTree<pcl::PointXYZ>);
@@ -180,113 +163,20 @@ int main (int argc, char** argv) {
     }
     **/
 
-
-    // Adjacency Graph
-    typedef  boost::adjacency_list<boost::setS, boost::setS, boost::undirectedS, pcl::PointXYZI, float> Graph;
-    Graph adjacency_graph;
-    octree.computeVoxelAdjacencyGraph(adjacency_graph);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_2 = erode_cloud(adjacency_graph,cloud_i);
-
-    /**
-    Graph::vertex_iterator vertexIt, vertexEnd;
-    Graph::adjacency_iterator neighbourIt, neighbourEnd;
-    boost::tie(vertexIt, vertexEnd) = boost::vertices(adjacency_graph);
-    int cnt = 0;
-    for (; vertexIt != vertexEnd; ++vertexIt) {
-        cout << *vertexIt << " is connected with ";
-        boost::tie(neighbourIt, neighbourEnd) = boost::adjacent_vertices(*vertexIt, adjacency_graph);
-        for (; neighbourIt != neighbourEnd; ++neighbourIt)
-            cout << *neighbourIt << " ";
-        cout << "\n"; cnt++;
-    }
-    cout << "Voxel: "<< cnt << endl;
-    cout << "Points: "<< cloud->size() << endl;
-    **/
-
-    // Voxel visualization stuff
-    /**
-    typedef std::vector< PointT, Eigen::aligned_allocator<PointT> > AlignedPointTVector;
-    AlignedPointTVector voxelCenters;
-
-    voxelCenters.clear();
-
-    octree.getOccupiedVoxelCenters(voxelCenters);
-    double voxelSideLen = std::sqrt(octree.getVoxelSquaredSideLen());
-
-    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-
-// Draw Voxels
-
-    vtkSmartPointer<vtkAppendPolyData> appendFilter = vtkSmartPointer<vtkAppendPolyData>::New ();
-
-    // Create every cubes to be displayed
-
-    for(int i = 0; i < voxelCenters.size (); i++) {
-        auto s = static_cast<float> (voxelSideLen / 2.0);
-        float x = voxelCenters[i].x;
-        float y = voxelCenters[i].y;
-        float z = voxelCenters[i].z;
-
-        vtkSmartPointer<vtkCubeSource> wk_cubeSource = vtkSmartPointer<vtkCubeSource>::New();
-
-        wk_cubeSource->SetBounds(x - s, x + s, y - s, y + s, z - s, z + s);
-        wk_cubeSource->Update();
-
-        appendFilter->AddInputData(wk_cubeSource->GetOutput());
-    }
-    // Remove any duplicate points
-    vtkSmartPointer<vtkCleanPolyData> cleanFilter = vtkSmartPointer<vtkCleanPolyData>::New ();
-
-    cleanFilter->SetInputConnection (appendFilter->GetOutputPort ());
-    cleanFilter->Update ();
-
-    //Create a mapper and actor
-    vtkSmartPointer<vtkPolyDataMapper> multiMapper = vtkSmartPointer<vtkPolyDataMapper>::New ();
-
-    multiMapper->SetInputConnection (cleanFilter->GetOutputPort ());
-
-    vtkSmartPointer<vtkActor> multiActor = vtkSmartPointer<vtkActor>::New ();
-
-    multiActor->SetMapper (multiMapper);
-
-    multiActor->GetProperty ()->SetColor (1.0, 1.0, 1.0);
-    multiActor->GetProperty ()->SetAmbient (1.0);
-    multiActor->GetProperty ()->SetLineWidth (0.1);
-    multiActor->GetProperty ()->EdgeVisibilityOn ();
-    multiActor->GetProperty ()->SetOpacity (1.0);
-
-    multiActor->GetProperty ()->SetRepresentationToWireframe ();
-
-    viewer->getRenderWindow()->GetRenderers()->GetFirstRenderer()->AddActor(multiActor);
-
-    viewer->addPointCloud<PointT>(cloud, "color_cloud");
-    viewer->setBackgroundColor (0, 0, 0);
-    viewer->setPointCloudRenderingProperties(
-            pcl::visualization::PCL_VISUALIZER_POINT_SIZE,3,"color_cloud");
-    viewer->setCameraPosition(0,0,0,1,0,1);
-
-    while (!viewer->wasStopped ()){
-        viewer->spinOnce();
-    }
-    **/
-
     // Normal visualization stuff
-//    /**
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3d cloud"));
-    pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity (cloud_2,"intensity");
+    pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity (cloud_g,"intensity");
     viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "cloud");
-    viewer->addPointCloud<pcl::PointXYZI>(cloud_2,intensity,"cloud");
-    viewer->addCoordinateSystem (1.0);
+    viewer->addPointCloud<pcl::PointXYZI>(cloud_g,intensity,"cloud");
     viewer->setBackgroundColor (0, 0, 0);
+    viewer->removeOrientationMarkerWidgetAxes();
     viewer->initCameraParameters ();
-
-//    pcl::io::savePCDFile("lucy_bin.pcd",*cloud_i);
-    pcl::io::savePCDFile("lucy_erode.pcd",*cloud_2);
 
     while (!viewer->wasStopped ()){
         viewer->spinOnce(100);
         boost::this_thread::sleep (boost::posix_time::microseconds (100000));
     }
-//    **/
+
+
     return (0);
 }
